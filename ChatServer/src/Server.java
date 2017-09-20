29c5c0d104c897a6d46287665249b41d23dae00b
Manh Nguyen <manhnguyenhuu7898@yahoo.com.vn>
2017-09-20 00:08:19 -0400

    update chatServer

--- ChatServer/src/Server.java
+++ ChatServer/src/Server.java
@@ -32,27 +32,43 @@ public class Server {
                                 byte b = dIS.readByte();
                                 if (b == (byte) ('|')) {
                                     String text = new String(byteStream, "UTF-8");
-                                    if (text.substring(0, 3).equals("*u*")){
-                                        // if username
+                                    if (text.substring(0, 3).equals("*u*")){// if username
                                         username = text.substring(3, index);
                                     }
-                                    else if (text.substring(0, 3).equals("*p*")){
-                                        // if password
+                                    else if (text.substring(0, 3).equals("*p*")){// if password
                                         password = text.substring(3, index);
-                                        if (checkLogin(username, password)){
-                                            // if password and username match
+                                        if (checkUserLogin(username, password)){// if password and username match
                                             activeUsers.add(new User(dIS, dOS, username));
                                             System.out.println("Logged in successfully for user " + username);
                                             // send confirmation to client so that client opens to user
                                             dOS.write(("%s%" + "t" + "|").getBytes(Charset.forName("UTF-8")));
                                             // for each active user, send this new username string to them
-                                            for (User onlineUser: activeUsers){
-                                                onlineUser.dOS.write(("%s%" + username + "|").getBytes(Charset.forName("UTF-8")));
+                                            for (int i=0; i<activeUsers.size()-1; i++){
+                                                activeUsers.get(i).dOS.write(("%s%" + username + "|").getBytes(Charset.forName("UTF-8")));
+                                                dOS.write(("%s%" + activeUsers.get(i).userName + "|").getBytes(Charset.forName("UTF-8")));
                                             }
                                         }
                                         else {
                                             // if password and username not match, send to client to print out error message
                                             dOS.write(("%s%" + "f" + "|").getBytes(Charset.forName("UTF-8")));
+                                        }
+                                    }
+                                    else if (text.substring(0, 3).equals("^u^")){
+                                        username = text.substring(3, index);
+                                    }
+                                    else if (text.substring(0, 3).equals("^p^")){
+                                        password = text.substring(3, index);
+                                        if (!checkUserSignUp(username)){
+                                            // if user is not created
+                                            // add new username and password to database
+                                            database.update(String.format(
+                                                    "INSERT INTO users (username, password) VALUES ('%s', '%s');", username, password));
+                                            activeUsers.add(new User(dIS, dOS, username));
+                                            System.out.println("Sign up successfully for user " + username);
+                                            // send confirmation to client so that clients open to user
+                                        }
+                                        else {
+                                            // if user is already created
                                         }
                                     }
                                     else {
@@ -79,9 +95,13 @@ public class Server {
         }
     }
 
-    public static boolean checkLogin(String usn, String pw) throws Exception{
+    public static boolean checkUserLogin(String usn, String pw) throws Exception{
         ResultSet user = database.query(
                 String.format("SELECT * FROM users WHERE name = '%s' AND password = '%s'", usn, pw));
+        return user.next();
+    }
+    public static boolean checkUserSignUp(String usn) throws Exception{
+        ResultSet user = database.query(String.format("SELECT * FROM users WHERE name = '%s'", usn));
         return user.next();
     }
 }
